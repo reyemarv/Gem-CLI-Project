5790aea3ce3e6eaa8c3ff430da76f85e65978518
reyemarv <reyemarv@gmail.com>
2020-03-18 19:13:31 -0500

    added required items

--- lib/InstaCLI.rb
+++ lib/InstaCLI.rb
@@ -1,8 +1,13 @@
 #environment file
 #load all files needed to run app
 
+require "nokogiri"
+require "open-uri"
+
 require "InstaCLI/version"
 require "InstaCLI/cli"
+
+require "pry"
 
 module InstaCLI
   class Error < StandardError; end
